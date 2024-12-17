4a03d6e8492a0f54fc4f61db4122199416395536
Priyanka Katre <priyankack1707@gmail.com>
2024-12-17 21:19:49 +0000

    clerk setup

--- wealth/src/app/layout.js
+++ wealth/src/app/layout.js
@@ -12,19 +12,21 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-  return (
-      <html lang="en">
+    return (
+      <ClerkProvider>
+        <html lang="en">
           <body className={`${inter.className}`}>
-              <Header />
-              <main className="min-h-screen">{children}</main>
-              <Toaster richColors />
+            <Header />
+            <main className="min-h-screen">{children}</main>
+            <Toaster richColors />
 
-              <footer className="bg-blue-50 py-12">
-                  <div className="container mx-auto px-4 text-center text-gray-600">
-                      <p>Made with 💗 by RoadsideCoder</p>
-                  </div>
-              </footer>
+            <footer className="bg-blue-50 py-12">
+              <div className="container mx-auto px-4 text-center text-gray-600">
+                <p>Made with 💗 by RoadsideCoder</p>
+              </div>
+            </footer>
           </body>
-      </html>
-  );
+        </html>
+      </ClerkProvider>
+    );
 }
--- wealth/src/components/header.jsx
+++ wealth/src/components/header.jsx
@@ -1,12 +1,18 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs';
 import React from 'react'
 
 
 const Header = () => {
   return (
     <div>
-        Header
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
-  )
+  );
 }
 
 export default Header
